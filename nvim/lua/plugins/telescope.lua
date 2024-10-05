af5c8c2ea831a726fa503ccb18aaed34f812ee7d
Syed Adeed <syedadeed@protonmail.com>
2024-10-05 19:14:56 +0530

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,9 @@ return {
 		"nvim-telescope/telescope.nvim", tag = "0.1.5",
 		dependencies = {"nvim-lua/plenary.nvim"},
 		config = function()
+            require('telescope').setup{
+                defaults = { file_ignore_patterns = {"env", "__pycache__"} }
+            }
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-Space>", builtin.find_files, {})
 		end
