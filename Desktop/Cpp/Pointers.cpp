8a928d0d60f58c9b210b3327a6024b7413f36228
Syed Adeed <syedadeed@protonmail.com>
2024-11-02 01:53:12 +0530

    update

--- Desktop/Cpp/Pointers.cpp
+++
@@ -1,5 +0,0 @@
-//When you are reading the declaration of a C/C++ variable, read it right to left in English and the meaning is obvious
-//e.g. What is a variable defined as ... int * const * 
-//Backward .. it is a Pointer to a constant pointer to an integer
-//e.g. What is a variable defined as int [] ** 
-//Backward .. it is a Pointer to a Pointer to an Array of Integers
