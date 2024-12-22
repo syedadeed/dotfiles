1dd15fc5954f0a7c6b370079c4e5ba7c7463236f
Syed Adeed <syedadeed@protonmail.com>
2024-12-22 22:31:20 +0530

    update

--- nvim/lua/config/key_binds.lua
+++ nvim/lua/config/key_binds.lua
@@ -1,11 +1,13 @@
-vim.keymap.set('n', '<Tab>', ':bn<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-w>', ':bdelete!<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-t>', ':enew<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-d>", ":Run<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-e>", ":Neotree filesystem toggle left<CR>", {silent = true})
 vim.keymap.set("n", "<C-Space>", ":Telescope find_files<CR>", {silent = true})
+
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "D", vim.lsp.buf.definition, {})
 vim.keymap.set({"n", "v"}, "C", vim.lsp.buf.code_action, {})
 
--- keymaps can also be found in treesitter.lua
+vim.keymap.set('n', '<Tab>', ':bn<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-w>', ':bdelete!<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-t>', ':enew<CR>', { noremap = true, silent = true })
+
+-- keymaps can also be found in treesitter.lua(<Leader>(ss,sn,sp,sc), [operation][{a/i}f,c,l,e])
