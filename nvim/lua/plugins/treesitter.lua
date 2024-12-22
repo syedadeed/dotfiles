2622ac46abe111771267466aaf653a94bbade297
Syed Adeed <syedadeed@protonmail.com>
2024-12-22 19:20:32 +0530

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,21 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			highlight = {enabled = true},
-			indent = {enabled = true},
-            fold = {enable = true}
-		})
-	end
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            highlight = {enabled = true},
+            indent = {enabled = true},
+            fold = {enable = true},
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            }
+        })
+    end
 }
