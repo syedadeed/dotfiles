259ed9e224722e139945aa2765555994d2a50f69
Syed Adeed <syedadeed@protonmail.com>
2024-12-22 20:04:43 +0530

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -15,9 +15,31 @@ return {
                         node_incremental = "<Leader>si",
                         scope_incremental = "<Leader>sc",
                         node_decremental = "<Leader>sd",
-                    },
+                    }
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v',
+                            ['@function.outer'] = 'V',
+                            ['@class.outer'] = '<c-v>',
+                        },
+                        include_surrounding_whitespace = true
+                    }
                 }
             })
         end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects"
     }
 }
