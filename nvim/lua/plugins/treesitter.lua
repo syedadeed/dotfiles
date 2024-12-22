1056f43b7210fd71f6c552d3afb9575310d7a81d
Syed Adeed <syedadeed@protonmail.com>
2024-12-22 19:54:16 +0530

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,23 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            auto_install = true,
-            highlight = {enabled = true},
-            indent = {enabled = true},
-            fold = {enable = true},
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<Leader>ss",
-                    node_incremental = "<Leader>si",
-                    scope_incremental = "<Leader>sc",
-                    node_decremental = "<Leader>sd",
-                },
-            }
-        })
-    end
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                auto_install = true,
+                highlight = {enabled = true},
+                indent = {enabled = true},
+                fold = {enable = true},
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<Leader>ss",
+                        node_incremental = "<Leader>si",
+                        scope_incremental = "<Leader>sc",
+                        node_decremental = "<Leader>sd",
+                    },
+                }
+            })
+        end
+    }
 }
