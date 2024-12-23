4f15fef9afe2e1dec27874578fd3d9c3a7e73208
Syed Adeed <syedadeed@protonmail.com>
2024-12-23 19:27:08 +0530

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,11 +1,10 @@
 return {
     "williamboman/mason-lspconfig.nvim",
     dependencies = {
-        "williamboman/mason.nvim",
+        {"williamboman/mason.nvim", opts = {}},
         "neovim/nvim-lspconfig"
     },
     config = function()
-        require("mason").setup()
         require("mason-lspconfig").setup_handlers({
             function (server_name)
                 local capabs = require("cmp_nvim_lsp").default_capabilities()
