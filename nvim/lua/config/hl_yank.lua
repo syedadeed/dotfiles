bb83f94f6a2d4e182e620bbde30c6d7e213a6b6e
Syed Adeed <syedadeed@protonmail.com>
2024-12-23 19:09:10 +0530

    upd

--- nvim/lua/config/hl_yank.lua
+++ nvim/lua/config/hl_yank.lua
@@ -2,12 +2,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.api.nvim_set_hl(0, 'CustomYankHighlight', {
       fg = '#ffffff',
-      bg = '#32557d',
+      bg = '#32557d'
     })
 
     vim.highlight.on_yank({
       higroup = 'CustomYankHighlight',
-      timeout = 150,
+      timeout = 150
     })
   end
 })
