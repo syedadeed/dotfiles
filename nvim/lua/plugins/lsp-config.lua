43cf900337da3a88b6623f0a60126ac42e9c913e
Syed Adeed <syedadeed@protonmail.com>
2024-12-23 09:16:00 +0530

    update

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return{
         config = function()
             require("mason-lspconfig").setup_handlers({
                 function (server_name)
-                    require("lspconfig")[server_name].setup({})
+                    require("lspconfig")[server_name].setup({capabilities = require("cmp_nvim_lsp").default_capabilities()})
                 end
             })
         end
