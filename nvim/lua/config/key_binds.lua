ca1914f61cfe84b36743c3d1b66efe85195d2066
Syed Adeed <syedadeed@protonmail.com>
2024-12-23 20:33:29 +0530

    update

--- nvim/lua/config/key_binds.lua
+++ nvim/lua/config/key_binds.lua
@@ -60,3 +60,6 @@ vim.keymap.set("n", "<C-h>", "<C-W>h", {silent = true})
 vim.keymap.set("n", "<C-j>", "<C-W>j", {silent = true})
 vim.keymap.set("n", "<C-k>", "<C-W>k", {silent = true})
 vim.keymap.set("n", "<C-l>", "<C-W>l", {silent = true})
+
+vim.keymap.set("n", "<C-n>", ":cnext<CR>", {silent = true})
+vim.keymap.set("n", "<C-p>", ":cprevious<CR>", {silent = true})
