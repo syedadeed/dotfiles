73d97b39637c395e768698631d51bb6caf2126c0
Syed Adeed <syedadeed@protonmail.com>
2025-03-19 17:20:54 +0530

    add support for snippets for html autocompletions

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -3,11 +3,23 @@ return {
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path"
+        "hrsh7th/cmp-path",
+        {
+            "L3MON4D3/LuaSnip",
+            dependencies = {"saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets"},
+            config = function ()
+                require("luasnip.loaders.from_vscode").lazy_load()
+            end
+        }
     },
     config = function()
         local cmp = require("cmp")
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end
+            },
             window = {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
@@ -21,6 +33,7 @@ return {
             }),
             sources = cmp.config.sources({
                 {name = "nvim_lsp"},
+                {name = "luasnip"},
                 {name = "buffer"},
                 {name = "path"}
             })
