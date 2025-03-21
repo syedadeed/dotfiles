d55678fc4e856dcefb9ccede8c447467117729f3
Syed Adeed <syedadeed@protonmail.com>
2025-03-22 01:56:52 +0530

    More backlog

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,6 @@ require("config.key_binds")
 vim.cmd.colorscheme("ayu-dark")
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {virtual_text = false })
 -- TODO: neo tree, telescope, ui_improvements, git_signs
+-- Linting and formatting => https://youtu.be/y1WWOaLCNyI?feature=shared
+-- snippets => https://youtu.be/Dn800rlPIho?feature=shared
+-- debuggers
