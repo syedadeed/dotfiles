7b0c80a06ffa54807daaa93c024d5c1dc20837c7
Syed Adeed <syedadeed@protonmail.com>
2025-03-22 01:21:43 +0530

    why

--- nvim/lua/config/lazy_setup.lua
+++ nvim/lua/config/lazy_setup.lua
@@ -15,5 +15,5 @@ require("lazy").setup({
         {import = "plugins"},
         {import = "plugins.core"}
     },
-    {change_detection = {notify = false}}
+    change_detection = {notify = false}
 })
