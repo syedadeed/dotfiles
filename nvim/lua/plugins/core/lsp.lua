e13e98ba402b6f8058792a56fd2d004d51555607
Syed Adeed <syedadeed@protonmail.com>
2026-01-18 18:30:50 +0530

    v 0.11 update stuff

--- nvim/lua/plugins/core/lsp.lua
+++ nvim/lua/plugins/core/lsp.lua
@@ -6,29 +6,12 @@ return {
         "b0o/schemastore.nvim"
     },
     config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local schema_store = require("schemastore")
-        require("mason-lspconfig").setup({
-            function (server_name)
-                require("lspconfig")[server_name].setup({capabilities = capabilities})
-            end
-        })
-        vim.lsp.config("lua_ls", {
-            capabilities = capabilities,
-            settings = {Lua = {workspace = {library = vim.api.nvim_get_runtime_file("", true)}}}
-        })
-        vim.lsp.config("ts_ls", {
-            capabilities = capabilities,
-            init_options = {preferences = {disableSuggestions = true}}
-        })
-        vim.lsp.config("jsonls", {
-            capabilities = capabilities,
-            settings = {json = {schemas = schema_store.json.schemas()}}
-        })
-        vim.lsp.config("yamlls", {
-            capabilities = capabilities,
-            settings = {yaml = {schemaStore = {enable = false, url = ""}, schemas = schema_store.yaml.schemas()}}
-        })
+        vim.lsp.config("lua_ls", {settings = {Lua = {workspace = {library = vim.api.nvim_get_runtime_file("", true)}}}})
+        vim.lsp.config("ts_ls", {init_options = {preferences = {disableSuggestions = true}}})
+        vim.lsp.config("jsonls", {settings = {json = {schemas = schema_store.json.schemas()}}})
+        vim.lsp.config("yamlls", {settings = {yaml = {schemaStore = {enable = false, url = ""}, schemas = schema_store.yaml.schemas()}}})
+        require("mason-lspconfig").setup()
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
         vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition, {})
         vim.keymap.set("n", "<Leader>gr", vim.lsp.buf.references, {})
