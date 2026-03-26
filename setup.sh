bfd1a9c9d93aba5fe0ab57803de6d945b77ba2ea
Syed Adeed <syedadeed@protonmail.com>
2026-03-26 22:00:58 +0530

    fixing past sins

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-rm .bash_profile
-rm .bashrc
-ln -s /home/adeed/dotfiles/bash_profile /home/adeed/.bash_profile
-ln -s /home/adeed/dotfiles/bashrc /home/adeed/.bashrc
-ln -s /home/adeed/dotfiles/fonts /home/adeed/.local/share
-ln -s /home/adeed/dotfiles/gitconfig /home/adeed/.gitconfig
-ln -s /home/adeed/dotfiles/gtk-3.0 /home/adeed/.config
-ln -s /home/adeed/dotfiles/hypr /home/adeed/.config
-ln -s /home/adeed/dotfiles/kitty /home/adeed/.config
-ln -s /home/adeed/dotfiles/nvim /home/adeed/.config
-ln -s /home/adeed/dotfiles/xremap /home/adeed/.config
-mkdir -p /home/adeed/.config/libvirt
-ln -s /home/adeed/dotfiles/libvirt/libvirt.conf /home/adeed/.config/libvirt/libvirt.conf
+rm "$HOME/.bash_profile"
+rm "$HOME/.bashrc"
+
+ln -s "$HOME/dotfiles/bash_profile" "$HOME/.bash_profile"
+ln -s "$HOME/dotfiles/bashrc" "$HOME/.bashrc"
+ln -s "$HOME/dotfiles/fonts" "$HOME/.local/share/fonts"
+ln -s "$HOME/dotfiles/gitconfig" "$HOME/.gitconfig"
+
+mkdir -p "$HOME/.config"
+
+ln -s "$HOME/dotfiles/gtk-3.0" "$HOME/.config/gtk-3.0"
+ln -s "$HOME/dotfiles/hypr" "$HOME/.config/hypr"
+ln -s "$HOME/dotfiles/kitty" "$HOME/.config/kitty"
+ln -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+ln -s "$HOME/dotfiles/xremap" "$HOME/.config/xremap"
+
+mkdir -p "$HOME/.config/libvirt"
+ln -s "$HOME/dotfiles/libvirt/libvirt.conf" "$HOME/.config/libvirt/libvirt.conf"
